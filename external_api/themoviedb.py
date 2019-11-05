38666567754a1b288f0cdcae214eca6cdfe215a8
rroyer <russellroyer@gmail.com>
2019-11-04 22:11:03 -0600

    added method comments

--- external_api/themoviedb.py
+++ external_api/themoviedb.py
@@ -2,17 +2,48 @@ import requests
 import urllib.parse
 
 class TheMovieDBAPI:
+    """
+    Static class for wrapping requests to the Movie DB external API.
+    Documentation for themoviedb api search endpoints can be found here:
+    https://developers.themoviedb.org/3/search/search-movies
+    
+    Attributes:
+    api_key (string): API key currently configured to use in the external API requests
+    api_version (string): Version number of the API the requests should use.
+    base_url (string): The base url of this external API.
+    """
+    api_key = None
     api_version = "4"
     base_url = "https://api.themoviedb.org/"
 
     @staticmethod
     def init(api_key):
+        """
+        Initializes the external api class with an api key configured by the application.
+        
+        Paramters:
+        api_key (string): API key currently configured to use in the external API requests
+        """
         if api_key is None or not len(api_key):
             raise Exception("Missing TheMovieDB API key")
         TheMovieDBAPI.api_key = api_key
     
     @staticmethod
     def lookup_movie(query_text, page_num=None, include_adult=None, year=None, primary_release_year=None):
+        """
+        Formats and sends a query to the Movie DB external API and returns the results. The primary query is text, but if
+        any other search paramters are included, they will be passed on to the API.
+        
+        Paramters:
+        query_text (string): Text of the query used to search.
+        page_num (int): The page number to be requested from the API.
+        include_adult (bool): Whether to include mature rated titles
+        year (int): Year to search titles.
+        primary_release_year (int): Primary release year to search titles
+        
+        Returns:
+        Dictionary: JSON results returned from the external API.
+        """
         path = "/search/movie"
         params = {"query": query_text}
         if page_num is not None:
@@ -33,6 +64,16 @@ class TheMovieDBAPI:
     
     @staticmethod
     def send_request(path, params):
+        """
+        Formats and sends a request to the Movie DB external API on a given path and passes the paramters, including the api_key.
+        
+        Paramters:
+        path (string): Path of the API to search.
+        params (int): Any query string paramters to include.
+        
+        Returns:
+        requests.models.Response: Object returned by the http request.
+        """
         params["api_key"] = TheMovieDBAPI.api_key
         request_url = TheMovieDBAPI.base_url + TheMovieDBAPI.api_version + path
         params = urllib.parse.urlencode(params)
